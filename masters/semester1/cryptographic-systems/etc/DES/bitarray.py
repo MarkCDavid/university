f0d5eb4b248abcbed83cf744a70486398fff5dd1
Aurimas Sakalys <sakalysaurimas@gmail.com>
2022-09-20 00:31:09 +0300

    SDES base

---
+++ masters/semester1/cryptographic-systems/etc/DES/bitarray.py
@@ -0,0 +1,111 @@
+from typing import Tuple, Union
+from math import ceil
+
+class BitArray:
+
+    def __init__(self, size: 'int', bits: 'int') -> 'None':
+        self.size = size
+        self.bits = bits
+
+    @staticmethod
+    def fromString(string: 'str', encoding: 'str') -> 'BitArray':
+        bytes = string.encode(encoding)
+        return BitArray(len(bytes) * 8, int.from_bytes(bytes, "little"))
+
+    @staticmethod
+    def fromBitArray(bitArray: 'BitArray') -> 'BitArray':
+        return BitArray(bitArray.size, bitArray.bits)
+
+    @staticmethod
+    def empty(size: 'int') -> 'BitArray':
+        return BitArray(size, 0)
+
+    def __add__(self: 'BitArray', other: 'BitArray') -> 'BitArray':
+        result = BitArray.empty(self.size + other.size)
+        for sourceIndex, targetIndex in enumerate(range(self.size)):
+            result[targetIndex] = self[sourceIndex]
+
+        for sourceIndex, targetIndex in enumerate(range(self.size, self.size + other.size)):
+            result[targetIndex] = other[sourceIndex]
+        
+        return result
+
+    def __xor__(self: 'BitArray', other: 'BitArray') -> 'BitArray':
+        size = min(self.size, other.size)
+        result = BitArray.empty(size)
+
+        for index in range(size):
+            result[index] = self[index] ^ other[index]
+        
+        return result
+
+    def __lshift__(self: 'BitArray', amount: 'int') -> 'BitArray':
+        return self._circular_shift(-amount)
+
+    def __rshift__(self: 'BitArray', amount: 'int') -> 'BitArray':
+        return self._circular_shift(amount)
+
+    def _circular_shift(self: 'BitArray', amount: 'int') -> 'BitArray':
+        if self.size == 0:
+            return BitArray.empty(0)
+
+        result = BitArray.fromBitArray(self)
+        
+        for source in range(result.size):
+            target = (source + amount) % result.size
+            result[target] = self[source]
+
+        return result
+   
+    def __setitem__(self, index: 'int', value: 'int') -> 'None':
+        self._assert_index_within_range(index)
+        self._assert_value_within_range(value)
+        self._set(index) if value else self._unset(index)
+
+    def __getitem__(self, coordinate: 'Union[int, slice]') -> 'int':
+        if isinstance(coordinate, int):
+            return self.__getitem_index__(coordinate)
+
+        if isinstance(coordinate, slice):
+            return self.__getitem_slice__(coordinate)
+        
+        raise Exception()
+
+    def __getitem_index__(self, index: 'int') -> 'int':
+        assert index < self.size
+        return (self.bits >> index) & 1
+
+    def __getitem_slice__(self: 'BitArray', slice: 'slice') -> 'BitArray':
+        start, stop, step = self._unpack_silce(slice)
+        bit_count = ceil((stop - start) / step)
+
+        result = BitArray(bit_count, 0)
+        for targetIndex, sourceIndex in enumerate(range(start, stop, step)):
+            result[targetIndex] = self[sourceIndex]
+        
+        return result
+
+    def _set(self, index) -> None:
+        self.bits |= (1 << index)
+
+    def _unset(self, index) -> None:
+        self.bits &= (~(1 << index))
+
+    def _unpack_silce(self, slice: 'slice') -> 'Tuple[int, int, int]':
+        start, stop, step = slice.start or 0, slice.stop or self.size, slice.step or 1
+        self._assert_index_within_range(start)
+        self._assert_index_within_range(stop)
+        start, stop = (stop, start - 1) if step < 0 else (start, stop)
+        return start, stop, step
+
+    def _assert_index_within_range(self, index: 'int'):
+        assert index >= 0 and index <= self.size
+
+    def _assert_value_within_range(self, value: 'int'):
+        assert value == 0 or value == 1
+    
+    def __str__(self) -> 'str':
+        return ''.join(str(self[index]) for index in range(self.size))
+
+    def __len__(self) -> 'int':
+        return self.size
---
+++ masters/semester1/cryptographic-systems/etc/DES/sdes.py
@@ -0,0 +1,75 @@
+from pydoc import plain
+from typing import List
+from bitarray import BitArray
+
+P10 = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]
+P8 = [5, 2, 6, 3, 7, 4, 9, 8]
+P4 = [1, 3, 2, 0]
+IP8 = [1, 5, 2, 0, 3, 7, 4, 6]
+IP8_R = [3, 0, 2, 4, 6, 1, 7, 5]
+EP =  [3, 0, 1, 2, 1, 2, 3, 0]
+
+def permute(permutation: 'List[int]', bits: 'BitArray') -> 'BitArray':
+    result = BitArray.empty(len(permutation))
+    for targetIndex, sourceIndex in enumerate(permutation):
+        result[targetIndex] = bits[sourceIndex]
+    return result
+
+S0 = [
+    [BitArray(2, 0b01), BitArray(2, 0b00), BitArray(2, 0b11), BitArray(2, 0b10)],
+    [BitArray(2, 0b11), BitArray(2, 0b10), BitArray(2, 0b01), BitArray(2, 0b00)],
+    [BitArray(2, 0b00), BitArray(2, 0b10), BitArray(2, 0b01), BitArray(2, 0b11)],
+    [BitArray(2, 0b11), BitArray(2, 0b01), BitArray(2, 0b11), BitArray(2, 0b10)]
+]
+
+S1 = [
+    [BitArray(2, 0b00), BitArray(2, 0b01), BitArray(2, 0b10), BitArray(2, 0b11)],
+    [BitArray(2, 0b10), BitArray(2, 0b00), BitArray(2, 0b01), BitArray(2, 0b11)],
+    [BitArray(2, 0b11), BitArray(2, 0b00), BitArray(2, 0b01), BitArray(2, 0b00)],
+    [BitArray(2, 0b10), BitArray(2, 0b01), BitArray(2, 0b00), BitArray(2, 0b11)]
+]
+
+def s(source: 'List[List[BitArray]]', bits: 'BitArray') -> 'BitArray':
+    row = bits[0] * 2 + bits[3]
+    column = bits[1] * 2 + bits[2]
+    return source[row][column]
+
+def generate_subkeys(key: 'BitArray') -> 'List[BitArray]':
+    assert len(key) == 10
+    key = permute(P10, key)
+    left, right = key[:5], key[5:]
+
+    result = []
+    for offset in range(2):
+        left, right = left << offset, right << offset
+        key = permute(P8, left + right)
+        result.append(key)
+
+    return result
+
+def encrypt(plaintext: 'BitArray', subkeys: 'List[BitArray]') -> 'BitArray':
+    plaintext = permute(IP8, plaintext)
+    left, right = plaintext[:4], plaintext[4:]
+    ep = permute(EP, right) ^ subkeys[0]
+    ep_left, ep_right = s(S0, ep[:4]), s(S1, ep[4:])
+    p4 = permute(P4, ep_left + ep_right)
+    left = left ^ p4
+    left, right = right, left
+    ep = permute(EP, right) ^ subkeys[1]
+    ep_left, ep_right = s(S0, ep[:4]), s(S1, ep[4:])
+    p4 = permute(P4, ep_left + ep_right)
+    left = left ^ p4
+    return permute(IP8_R, left + right)
+
+
+key = BitArray(10, 0b0111100011)
+subkeys = generate_subkeys(key)
+for subkey in subkeys:
+    print("Subkey:", subkey)
+
+plaintext = BitArray(8, 0b00010100)
+ciphertext = encrypt(plaintext, subkeys)
+
+print("       Key:", key)
+print(" Plaintext:", plaintext)
+print("Ciphertext:", ciphertext)
