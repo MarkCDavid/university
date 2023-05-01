d8d296e228111d1eb91842b0f449a932951426bc
Aurimas Šakalys <aurimas.sakalys@stud.vilniustech.lt>
2023-05-01 15:39:48 +0300

    report

--- masters/semester2/cnoss/homework3/configure.sh
+++
@@ -1,4 +0,0 @@
-sudo ip link add link eno2 veno2_1 type macvlan mode bridge
-sudo ip addr add 192.168.56.10/24 dev veno2_1
-sudo ip route add default via 192.168.56.1 dev veno2_1
-sudo ip link set veno2_1 up
